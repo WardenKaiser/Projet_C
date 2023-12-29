4af69c669229b7abbba09f34e8a1c90d753104d6
GuillaumeLanciaux <104573147+GuillaumeLanciaux@users.noreply.github.com>
2023-12-29 01:52:34 +0100

    5

--- main.c
+++ main.c
@@ -3,11 +3,43 @@
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
+#include<time.h>
+#include<ncurses.h>
 
 int main(int argc, char const *argv[])
 {
-	
+	/*int x;
 	printf("print\n");
+clock_t start_time, end_time;
+
+    start_time = clock();
+
+    while ( x !=1000)
+	{
+	printf("print\n");
+	x++;
+	}
 	
+    
+    end_time = clock();
+
+    
+    double elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("Le temps écoulé est de %.2f secondes.\n", elapsed_time);
+	*/
+ int compteur = 0;
+    char touche;
+
+    while (1) {
+        touche = getch(); 
+
+        if (touche == 'q') {
+            printf("Sortie du programme.\n");
+            break; 
+        }
+
+        compteur++;
+        printf("Compteur : %d\n", compteur);
+    }
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 projet: projet.o main.o
-	gcc -std=c99 -o projet projet.o main.o
+	gcc -std=c99 -o projet projet.o main.o -lncurses
 
 projet.o: projet.c
-	gcc -std=c99 -c projet.c -o projet.o -W -Wall
+	gcc -std=c99 -c projet.c -o projet.o -W -lncurses
 
 main.o: main.c projet.h
-	gcc -std=c99 -c main.c -o main.o -W -Wall
+	gcc -std=c99 -c main.c -o main.o -W -lncurses
